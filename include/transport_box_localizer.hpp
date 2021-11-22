cbb1d2c1b115d8089cf4461d64f93b8db5279121
Yuying.Jin <jinyuyingsspu@gmail.com>
2021-11-22 18:38:38 +0800

    add icp

--- include/transport_box_localizer.hpp
+++ include/transport_box_localizer.hpp
@@ -1,34 +1,65 @@
 #ifndef TRANSPORT_BOX_LOCALIZER_H_
 #define TRANSPORT_BOX_LOCALIZER_H_
 
+#include <math.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <Eigen/Dense>
+#include <boost/algorithm/string.hpp>
+#include <boost/thread.hpp>
 #include <thread>
 
+#include "geometry_msgs/PoseArray.h"
 #include "pointmatcher/PointMatcher.h"
-
 using Point = pcl::PointXYZ;
 using Pointcloud = pcl::PointCloud<Point>;
 
+using namespace std;
+using namespace Eigen;
+
+typedef Eigen::Matrix<Eigen::Vector4d, Eigen::Dynamic, 1> List4DPoints;
+
 namespace transport_box_localizer {
+
+typedef union {
+    unsigned char cv[4];
+    float fv;
+} float_union;
+
+typedef struct {
+    float xy_coordinates[2];
+} box_legs;
+
+typedef PointMatcher<float> PM;
+typedef PM::DataPoints DP;
+
 class TransportBoxLocalizer {
 private:
     Pointcloud::Ptr loadPointcloudFromPcd(const std::string &filename);
     void publishCloud(Pointcloud::Ptr cloud, const ros::Publisher &pub, const std::string &frameId);
     void runBehavior(void);
-    void laserpointcallback(const sensor_msgs::PointCloud2::ConstPtr &pointMsgIn);
+    void lidarpointcallback(const sensor_msgs::PointCloud2::ConstPtr &pointMsgIn);
+    DP fromPCL(const Pointcloud &pcl);
 
     Pointcloud::Ptr mapCloud;
     std::thread *run_behavior_thread_;
 
     ros::Publisher cloudPub;
+    ros::Publisher laser_filtered_point_pub;
+    ros::Publisher box_legs_array_pub;
     ros::Subscriber cloudSub;
 
-    typedef PointMatcher<float> PM;
-    typedef PM::DataPoints DP;
+    double detect_up_, detect_down_, detect_right_, detect_left_, lidar_intensity_;
+
+    vector<box_legs> box_legs_;
+    vector<box_legs> cluster_box_legs_;
+
+    // Debug
+    geometry_msgs::PoseArray poseArray;
+    geometry_msgs::Pose box_legs_pose;
 
 public:
     TransportBoxLocalizer();
--- src/transport_box_localizer.cpp
+++ src/transport_box_localizer.cpp
@@ -4,12 +4,45 @@ TransportBoxLocalizer::TransportBoxLocalizer() {
     ros::NodeHandle nh("~");
     const std::string pclFilename = nh.param<std::string>("pcd_filename", "");
     mapCloud = loadPointcloudFromPcd(pclFilename);
+
+    nh.param("detect_up", detect_up_, 2.0);
+    nh.param("detect_down", detect_down_, -1.0);
+    nh.param("detect_right", detect_right_, 1.0);
+    nh.param("detect_left", detect_left_, 1.0);
+    nh.param("lidar_intensity", lidar_intensity_, 35000.0);
+
+    // Create the marker positions from the test points
+    List4DPoints positions_of_markers_on_object;
+    // Read in the marker positions from the YAML parameter file
+    XmlRpc::XmlRpcValue points_list;
+    if (!nh.getParam("marker_positions", points_list)) {
+        ROS_ERROR(
+            "%s: No reference file containing the marker positions, or the "
+            "file is improperly formatted. Use the 'marker_positions_file' "
+            "parameter in the launch file.",
+            ros::this_node::getName().c_str());
+        ros::shutdown();
+    } else {
+        positions_of_markers_on_object.resize(points_list.size());
+        for (int i = 0; i < points_list.size(); i++) {
+            Eigen::Matrix<double, 4, 1> temp_point;
+            temp_point(0) = points_list[i]["x"];
+            temp_point(1) = points_list[i]["y"];
+            temp_point(2) = points_list[i]["z"];
+            temp_point(3) = 1;
+            positions_of_markers_on_object(i) = temp_point;
+        }
+    }
     // publish
-    cloudPub = nh.advertise<sensor_msgs::PointCloud2>("icp_map", 1, true);
+    cloudPub = nh.advertise<sensor_msgs::PointCloud2>("icp_map", 10, true);
+    // laser_filtered_point_pub = nh.advertise<sensor_msgs::PointCloud2>("laser_filtered_point", 10);
+    box_legs_array_pub = nh.advertise<geometry_msgs::PoseArray>("box_legs", 10);
+
     // subscribe
     cloudSub = nh.subscribe<sensor_msgs::PointCloud2>(
-        "/tim551_cloud", 100, boost::bind(&TransportBoxLocalizer::laserpointcallback, this, _1));
+        "/tim551_cloud", 100, boost::bind(&TransportBoxLocalizer::lidarpointcallback, this, _1));
 
+    // const DP ref(DP::load(mapCloud));
     // Create the default ICP algorithm
     PM::ICP icp;
     // See the implementation of setDefault() to create a custom ICP algorithm
@@ -38,15 +71,114 @@ void TransportBoxLocalizer::runBehavior(void) {
     ros::NodeHandle nh;
     ros::Rate rate(15.0);
     while (nh.ok()) {
-        publishCloud(mapCloud, cloudPub, "world");
+        publishCloud(mapCloud, cloudPub, "laser_sick_tim551");
         rate.sleep();
     }
 }
+DP TransportBoxLocalizer::fromPCL(const Pointcloud &pcl) {
+    // todo this can result in data loss???
+    sensor_msgs::PointCloud2 ros;
+    pcl::toROSMsg(pcl, ros);
+    return pointmatcher_ros::rosMsgToPointMatcherCloud<float>(ros);
+}
 
-void TransportBoxLocalizer::laserpointcallback(const sensor_msgs::PointCloud2::ConstPtr &pointMsgIn) {
+void TransportBoxLocalizer::lidarpointcallback(const sensor_msgs::PointCloud2::ConstPtr &pointMsgIn) {
     if (pointMsgIn == nullptr) {
         return;
     }
+    sensor_msgs::PointCloud2 filtered_point;
+    float_union point_x, point_y, intensity;
+    vector<uint8_t> tmp_data;
+    uint32_t point_count = 0;
+    box_legs box_legs_coordinates;
+    for (int i = 0; i < pointMsgIn->width; i++) {
+        for (int j = 0; j < 4; j++) {
+            point_x.cv[j] = pointMsgIn->data[i * 16 + j];
+            point_y.cv[j] = pointMsgIn->data[i * 16 + 4 + j];
+            intensity.cv[j] = pointMsgIn->data[i * 16 + 12 + j];
+        }
+        if (point_x.fv > detect_down_ && point_x.fv < detect_up_ && fabs(point_y.fv) < detect_right_ &&
+            intensity.fv > lidar_intensity_) {
+            point_count++;
+            for (int z = 0; z < 16; z++) {
+                tmp_data.push_back(pointMsgIn->data[i * 16 + z]);
+            }
+            box_legs_coordinates.xy_coordinates[0] = point_x.fv;
+            box_legs_coordinates.xy_coordinates[1] = point_y.fv;
+            box_legs_.push_back(box_legs_coordinates);
+        }
+    }
+    if (box_legs_.size() == 0) {
+        box_legs_.clear();
+        return;
+    }
+    // debug
+    filtered_point.header = pointMsgIn->header;
+    filtered_point.height = pointMsgIn->height;
+    filtered_point.width = point_count;
+    filtered_point.fields = pointMsgIn->fields;
+    filtered_point.is_bigendian = pointMsgIn->is_bigendian;
+    filtered_point.point_step = pointMsgIn->point_step;
+    filtered_point.row_step = point_count * 16;
+    for (int i = 0; i < filtered_point.row_step; i++) {
+        filtered_point.data.push_back(tmp_data[i]);
+    }
+    ROS_DEBUG("point_count: %d", point_count);
+    laser_filtered_point_pub.publish(filtered_point);
+    // Clustering
+    float sum_x_pos = 0, sum_y_pos = 0;
+    uint32_t classify_number = 0;
+    box_legs cluster_box_legs_coordinates;
+    bool classify_flag = false;
+    for (int i = 0; i < box_legs_.size() - 1; i++) {
+        if (pow(pow(box_legs_[i].xy_coordinates[0] - box_legs_[i + 1].xy_coordinates[0], 2) +
+                    pow(box_legs_[i].xy_coordinates[1] - box_legs_[i + 1].xy_coordinates[1], 2),
+                0.5) < 0.05) {
+            sum_x_pos += box_legs_[i].xy_coordinates[0];
+            sum_y_pos += box_legs_[i].xy_coordinates[1];
+            classify_number++;
+            classify_flag = false;
+        } else {
+            if (classify_number >= 3) {
+                cluster_box_legs_coordinates.xy_coordinates[0] = sum_x_pos / classify_number;
+                cluster_box_legs_coordinates.xy_coordinates[1] = sum_y_pos / classify_number;
+
+                cluster_box_legs_.push_back(cluster_box_legs_coordinates);
+
+                box_legs_pose.position.x = cluster_box_legs_coordinates.xy_coordinates[0];
+                box_legs_pose.position.y = cluster_box_legs_coordinates.xy_coordinates[1];
+                box_legs_pose.position.z = 0;
+                poseArray.poses.push_back(box_legs_pose);
+            }
+            classify_number = 0;
+            sum_x_pos = 0;
+            sum_y_pos = 0;
+            classify_flag = true;
+        }
+    }
+    if (!classify_flag && classify_number >= 3) {
+        cluster_box_legs_coordinates.xy_coordinates[0] = sum_x_pos / classify_number;
+        cluster_box_legs_coordinates.xy_coordinates[1] = sum_y_pos / classify_number;
+
+        cluster_box_legs_.push_back(cluster_box_legs_coordinates);
+
+        box_legs_pose.position.x = cluster_box_legs_coordinates.xy_coordinates[0];
+        box_legs_pose.position.y = cluster_box_legs_coordinates.xy_coordinates[1];
+        box_legs_pose.position.z = 0;
+        poseArray.poses.push_back(box_legs_pose);
+    }
+    poseArray.header = pointMsgIn->header;
+    poseArray.header.stamp = ros::Time::now();
+    box_legs_array_pub.publish(poseArray);
+
+    for (int i = 0; i < cluster_box_legs_.size(); i++) {
+        ROS_INFO("cluster_box_legs_.xy_coordinates[0]:%f,cluster_box_legs_.box_legs.xy_coordinates[1]:%f",
+                 cluster_box_legs_[i].xy_coordinates[0], cluster_box_legs_[i].xy_coordinates[1]);
+    }
+
+    box_legs_.clear();
+    cluster_box_legs_.clear();
+    poseArray.poses.clear();
 }
 
 TransportBoxLocalizer::~TransportBoxLocalizer() {}
